99d9ed2ad45933658a44ceeed60eb2fbb65c3b8a
headlinecompares <headlinecompare@hlcompare.com>
2021-03-13 19:56:54 -0500

    settings to get static work in deployment

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -123,11 +123,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-"""
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-"""
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 print("base dir path", BASE_DIR)
