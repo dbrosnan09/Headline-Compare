a26db3045547d23fe31a5f3cf3a4029b5a6ba9cb
headlinecompares <headlinecompare@hlcompare.com>
2021-03-13 20:57:35 -0500

    kjhsdf

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -122,7 +122,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'scraper/static/'
+STATIC_URL = '/static/'
 
 
 
